76d1b676bf8603891f5cbbb8265ac260c9a4613e
gdgeek <dirui1@yeah.net>
2025-08-03 22:50:22 +0800

    no message

--- three.js/editor/js/Menubar.Add.js
+++ three.js/editor/js/Menubar.Add.js
@@ -8,7 +8,7 @@ import { VerseFactory } from './mrpp/VerseFactory.js';
 import { Builder } from './mrpp/Builder.js';
 
 
-function MenubarAdd( editor ) {
+function MenubarAdd(editor) {
 
 	const factory = new MetaFactory();
 
@@ -21,33 +21,33 @@ function MenubarAdd( editor ) {
 
 	const container = new UIPanel();
 
-	container.setClass( 'menu' );
+	container.setClass('menu');
 
 	const title = new UIPanel();
-	title.setClass( 'title' );
-	title.setTextContent( strings.getKey( 'menubar/add' ) );
-	container.add( title );
+	title.setClass('title');
+	title.setTextContent(strings.getKey('menubar/add'));
+	container.add(title);
 
 	const options = new UIPanel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass('options');
+	container.add(options);
 
 	// 存储所有创建的资源菜单项的映射
 	const resourceMenuItems = new Map();
 
 	let option = null;
 
-	if ( editor.type.toLowerCase() == 'meta' ) {
+	if (editor.type.toLowerCase() == 'meta') {
 
 		// 更新资源菜单项的可见性
-		const updateResourceMenuItems = function(availableTypes) {
+		const updateResourceMenuItems = function (availableTypes) {
 			if (!availableTypes || !Array.isArray(availableTypes)) return;
 
 			// 先隐藏所有资源菜单项
 			resourceMenuItems.forEach((menuItem) => {
 				menuItem.dom.style.display = 'none';
 			});
-
+			console.error(availableTypes)
 			// 显示可用的资源菜单项
 			availableTypes.forEach(type => {
 				const menuItem = resourceMenuItems.get(type);
@@ -57,14 +57,14 @@ function MenubarAdd( editor ) {
 			});
 		};
 
-		editor.signals.messageReceive.add( async function ( params ) {
+		editor.signals.messageReceive.add(async function (params) {
 
-			if ( params.action === 'load-resource' ) {
+			if (params.action === 'load-resource') {
 
 				const data = params.data;
 
 				// 将资源同时保存到本地和全局资源集合
-				resources.set( data.id.toString(), data );
+				resources.set(data.id.toString(), data);
 
 				// 添加到editor.resources
 				if (!editor.resources) editor.resources = [];
@@ -81,68 +81,73 @@ function MenubarAdd( editor ) {
 				}
 
 				// 创建对象
-				const raw = builder.resource( data );
-				if ( raw ) {
-					const node = await factory.building( raw, resources );
-					if ( node ) {
-						editor.execute( new AddObjectCommand( editor, node ) );
+				const raw = builder.resource(data);
+				if (raw) {
+					const node = await factory.building(raw, resources);
+					if (node) {
+						editor.execute(new AddObjectCommand(editor, node));
 					}
 				}
 
-			} else if ( params.action === 'available-resource-types' ) {
-				updateResourceMenuItems( params.data );
+			} else if (params.action === 'available-resource-types') {
+				updateResourceMenuItems(params.data);
 			}
 
-		} );
+		});
 
 		// Node
 		option = new UIRow();
-		option.setClass( 'option' );
-		option.setTextContent( strings.getKey( 'menubar/add/node' ) );
-		option.onClick( async function () {
+		option.setClass('option');
+		option.setTextContent(strings.getKey('menubar/add/node'));
+		option.onClick(async function () {
 
-			const node = await factory.building( builder.entity(), resources );
-			editor.execute( new AddObjectCommand( editor, node ) );
+			const node = await factory.building(builder.entity(), resources);
+			editor.execute(new AddObjectCommand(editor, node));
 
-		} );
-		options.add( option );
+		});
+		options.add(option);
 
 		// Text
 		option = new UIRow();
-		option.setClass( 'option' );
-		option.setTextContent( strings.getKey( 'menubar/add/text' ) );
-		option.onClick( async function () {
+		option.setClass('option');
+		option.setTextContent(strings.getKey('menubar/add/text'));
+		option.onClick(async function () {
 
-			const node = await factory.building( builder.text(), resources );
-			editor.execute( new AddObjectCommand( editor, node ) );
+			const node = await factory.building(builder.text(), resources);
+			editor.execute(new AddObjectCommand(editor, node));
 
-		} );
-		options.add( option );
+		});
+		options.add(option);
 
 		// 创建资源类型菜单项的函数
-		const createResourceMenuItem = function(resourceType) {
+		const createResourceMenuItem = function (resourceType) {
 			const menuItem = new UIRow();
-			menuItem.setClass( 'option' );
-			menuItem.setTextContent( strings.getKey( 'menubar/add/' + resourceType ) );
-			menuItem.onClick( async function () {
+			menuItem.setClass('option');
+			menuItem.setTextContent(strings.getKey('menubar/add/' + resourceType));
+			menuItem.onClick(async function () {
 				editor.signals.messageSend.dispatch(
 					{
 						action: 'load-resource',
 						data: { type: resourceType }
-					} );
-			} );
+					});
+			});
 			// 存储创建的菜单项
 			resourceMenuItems.set(resourceType, menuItem);
 			return menuItem;
 		};
 
 		// 初始创建所有可能的资源类型菜单项，默认先隐藏
-		const allPossibleResourceTypes = ['voxel', 'polygen', 'audio', 'picture', 'video'];
+		const allPossibleResourceTypes = ['-', 'voxel', 'polygen', 'audio', 'picture', 'video', '-', 'phototype'];
 		allPossibleResourceTypes.forEach(type => {
+			if (type === '-') {
+				options.add(new UIHorizontalRule());
+				return;
+			}
 			const menuItem = createResourceMenuItem(type);
 			menuItem.dom.style.display = 'none'; // 默认隐藏
 			options.add(menuItem);
 		});
+
 
 		// 请求获取可用的资源类型
 		editor.signals.messageSend.dispatch({
@@ -154,74 +159,74 @@ function MenubarAdd( editor ) {
 			updateResourceMenuItems(editor.availableResourceTypes);
 		}
 
-	} else if ( editor.type.toLowerCase() == 'verse' ) {
+	} else if (editor.type.toLowerCase() == 'verse') {
 
 		// const factory = new VerseFactory();
-		editor.signals.messageReceive.add( async function ( params ) {
+		editor.signals.messageReceive.add(async function (params) {
 
 
-			if ( params.action === 'add-module' ) {
+			if (params.action === 'add-module') {
 
 				const data = params.data.data;
 				const setup = params.data.setup;
 				const title = params.data.title;
 
-				console.error( data );
+				console.error(data);
 
-				if ( data.resources ) {
+				if (data.resources) {
 
-					data.resources.forEach( resource => {
+					data.resources.forEach(resource => {
 
-						resources.set( resource.id.toString(), resource );
+						resources.set(resource.id.toString(), resource);
 
-					} );
+					});
 
 				}
 
 
-				const node = factory.addModule( builder.module( data.id, title ) );
+				const node = factory.addModule(builder.module(data.id, title));
 
-				node.userData.data = JSON.stringify( setup );
+				node.userData.data = JSON.stringify(setup);
 				node.userData.custom = data.custom;
-				if ( data && data.data ) {
-					await factory.readMeta( node, data.data, resources, editor );
+				if (data && data.data) {
+					await factory.readMeta(node, data.data, resources, editor);
 				}
 
-				await factory.addGizmo( node );
-				editor.execute( new AddObjectCommand( editor, node ) );
+				await factory.addGizmo(node);
+				editor.execute(new AddObjectCommand(editor, node));
 
 			}
 
-		} );
+		});
 
 
 		// Meta
 		option = new UIRow();
-		option.setClass( 'option' );
-		option.setTextContent( strings.getKey( 'menubar/add/meta' ) );
-		option.onClick( async function () {
+		option.setClass('option');
+		option.setTextContent(strings.getKey('menubar/add/meta'));
+		option.onClick(async function () {
 
-			editor.signals.messageSend.dispatch( {
+			editor.signals.messageSend.dispatch({
 				action: 'add-meta'
-			} );
+			});
 
-		} );
-		options.add( option );
+		});
+		options.add(option);
 
 
 		// Prefabs
 		option = new UIRow();
-		option.setClass( 'option' );
-		option.setTextContent( strings.getKey( 'menubar/add/prefab' ) );
-		option.onClick( async function () {
+		option.setClass('option');
+		option.setTextContent(strings.getKey('menubar/add/prefab'));
+		option.onClick(async function () {
 
 			editor.signals.messageSend.dispatch(
 				{
 					action: 'add-prefab'
-				} );
+				});
 
-		} );
-		options.add( option );
+		});
+		options.add(option);
 
 	}
 
@@ -232,87 +237,87 @@ function MenubarAdd( editor ) {
 	// AmbientLight
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/ambientlight' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/ambientlight'));
+	option.onClick(function () {
 
 		const color = 0x222222;
 
-		const light = new THREE.AmbientLight( color );
+		const light = new THREE.AmbientLight(color);
 		light.name = 'AmbientLight';
 
-		editor.execute( new AddObjectCommand( editor, light ) );
+		editor.execute(new AddObjectCommand(editor, light));
 
-	} );
+	});
 	//options.add(option);
 
 	// DirectionalLight
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/directionallight' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/directionallight'));
+	option.onClick(function () {
 
 		const color = 0xffffff;
 		const intensity = 1;
 
-		const light = new THREE.DirectionalLight( color, intensity );
+		const light = new THREE.DirectionalLight(color, intensity);
 		light.name = 'DirectionalLight';
 		light.target.name = 'DirectionalLight Target';
 
-		light.position.set( 5, 10, 7.5 );
+		light.position.set(5, 10, 7.5);
 
-		editor.execute( new AddObjectCommand( editor, light ) );
+		editor.execute(new AddObjectCommand(editor, light));
 
-	} );
+	});
 	//options.add(option);
 
 	// HemisphereLight
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/hemispherelight' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/hemispherelight'));
+	option.onClick(function () {
 
 		const skyColor = 0x00aaff;
 		const groundColor = 0xffaa00;
 		const intensity = 1;
 
-		const light = new THREE.HemisphereLight( skyColor, groundColor, intensity );
+		const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
 		light.name = 'HemisphereLight';
 
-		light.position.set( 0, 10, 0 );
+		light.position.set(0, 10, 0);
 
-		editor.execute( new AddObjectCommand( editor, light ) );
+		editor.execute(new AddObjectCommand(editor, light));
 
-	} );
+	});
 	//options.add(option);
 
 	// PointLight
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/pointlight' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/pointlight'));
+	option.onClick(function () {
 
 		const color = 0xffffff;
 		const intensity = 1;
 		const distance = 0;
 
-		const light = new THREE.PointLight( color, intensity, distance );
+		const light = new THREE.PointLight(color, intensity, distance);
 		light.name = 'PointLight';
 
-		editor.execute( new AddObjectCommand( editor, light ) );
+		editor.execute(new AddObjectCommand(editor, light));
 
-	} );
+	});
 	//options.add(option);
 
 	// SpotLight
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/spotlight' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/spotlight'));
+	option.onClick(function () {
 
 		const color = 0xffffff;
 		const intensity = 1;
@@ -320,15 +325,15 @@ function MenubarAdd( editor ) {
 		const angle = Math.PI * 0.1;
 		const penumbra = 0;
 
-		const light = new THREE.SpotLight( color, intensity, distance, angle, penumbra );
+		const light = new THREE.SpotLight(color, intensity, distance, angle, penumbra);
 		light.name = 'SpotLight';
 		light.target.name = 'SpotLight Target';
 
-		light.position.set( 5, 10, 7.5 );
+		light.position.set(5, 10, 7.5);
 
-		editor.execute( new AddObjectCommand( editor, light ) );
+		editor.execute(new AddObjectCommand(editor, light));
 
-	} );
+	});
 	//options.add(option);
 
 	//
@@ -338,32 +343,32 @@ function MenubarAdd( editor ) {
 	// OrthographicCamera
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/orthographiccamera' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/orthographiccamera'));
+	option.onClick(function () {
 
 		const aspect = editor.camera.aspect;
-		const camera = new THREE.OrthographicCamera( - aspect, aspect );
+		const camera = new THREE.OrthographicCamera(- aspect, aspect);
 		camera.name = 'OrthographicCamera';
 
-		editor.execute( new AddObjectCommand( editor, camera ) );
+		editor.execute(new AddObjectCommand(editor, camera));
 
-	} );
+	});
 	//options.add(option);
 
 	// PerspectiveCamera
 
 	option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/perspectivecamera' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/perspectivecamera'));
+	option.onClick(function () {
 
 		const camera = new THREE.PerspectiveCamera();
 		camera.name = 'PerspectiveCamera';
 
-		editor.execute( new AddObjectCommand( editor, camera ) );
+		editor.execute(new AddObjectCommand(editor, camera));
 
-	} );
+	});
 	//options.add(option);
 
 	return container;
--- three.js/editor/js/mrpp/MetaFactory.js
+++ three.js/editor/js/mrpp/MetaFactory.js
@@ -12,29 +12,29 @@ const isHttps = () => {
 
 	const protocol = window.location.protocol;
 	const isHttps = protocol === 'https:';
-	console.log( isHttps ? '这个网页是使用HTTPS' : '这个网页不是使用HTTPS' );
+	console.log(isHttps ? '这个网页是使用HTTPS' : '这个网页不是使用HTTPS');
 	return isHttps;
 
 };
 
 // 将 URL 转换为 HTTPS 或 HTTP
-const convertToHttps = ( url ) => {
+const convertToHttps = (url) => {
 
-	if ( url === undefined || url === null ) return '';
+	if (url === undefined || url === null) return '';
 
-	if ( isHttps() ) {
+	if (isHttps()) {
 
-		if ( url.startsWith( 'http://' ) ) {
+		if (url.startsWith('http://')) {
 
-			return url.replace( 'http://', 'https://' );
+			return url.replace('http://', 'https://');
 
 		}
 
 	} else {
 
-		if ( url.startsWith( 'https://' ) ) {
+		if (url.startsWith('https://')) {
 
-			return url.replace( 'https://', 'http://' );
+			return url.replace('https://', 'http://');
 
 		}
 
@@ -51,73 +51,73 @@ class MetaFactory extends Factory {
 		super();
 
 	}
-	async addGizmo( node ) {
+	async addGizmo(node) {
 
-		return new Promise( resolve => {
+		return new Promise(resolve => {
 
-			const loader = new GLTFLoader( THREE.DefaultLoadingManager );
-			loader.load( '/three.js/mesh/unreal-gizmo.glb', gltf => {
+			const loader = new GLTFLoader(THREE.DefaultLoadingManager);
+			loader.load('/three.js/mesh/unreal-gizmo.glb', gltf => {
 
 				const mesh = gltf.scene;//.children[0]
-				mesh.scale.set( 0.1, 0.1, 0.1 );
-				mesh.rotation.set( Math.PI / 2, Math.PI / 2, 0 );
-				this.lockNode( gltf.scene );
-				node.add( gltf.scene );
+				mesh.scale.set(0.1, 0.1, 0.1);
+				mesh.rotation.set(Math.PI / 2, Math.PI / 2, 0);
+				this.lockNode(gltf.scene);
+				node.add(gltf.scene);
 				resolve();
 
-			} );
+			});
 
-		} );
+		});
 
 	}
 
-	addModule( data ) {
+	addModule(data) {
 
 		const node = new THREE.Group();
 		node.name = data.parameters.title;
-		console.error( 'addMetaData', data );
+		console.error('addMetaData', data);
 		node.type = data.type;
 		node.uuid = data.parameters.uuid;
 		node.visible = data.parameters.active;
 
 		const transform = data.parameters.transform;
-		this.setTransform( node, transform );
+		this.setTransform(node, transform);
 
 		const userData = {};
 
-		const exclude = [ 'name', 'title', 'uuid', 'transform', 'active' ];
+		const exclude = ['name', 'title', 'uuid', 'transform', 'active'];
 
-		Object.keys( data.parameters ).forEach( key => {
+		Object.keys(data.parameters).forEach(key => {
 
-			if ( ! exclude.includes( key ) ) {
+			if (!exclude.includes(key)) {
 
-				userData[ key ] = data.parameters[ key ];
+				userData[key] = data.parameters[key];
 
 			}
 
-		} );
+		});
 
 		userData.draggable = false;
 		node.userData = userData;
 		return node;
 
 	}
-	async readMeta( root, data, resources, editor = null ) {
+	async readMeta(root, data, resources, editor = null) {
 
 		//alert(resources.size)
-		if ( data.children ) {
+		if (data.children) {
 
-			for ( let i = 0; i < data.children.entities.length; ++ i ) {
+			for (let i = 0; i < data.children.entities.length; ++i) {
 
-				if ( data.children.entities[ i ] != null ) {
+				if (data.children.entities[i] != null) {
 
 					try {
 
-						const node = await this.building( data.children.entities[ i ], resources );
-						if ( node != null ) {
+						const node = await this.building(data.children.entities[i], resources);
+						if (node != null) {
 
-							root.add( node );
-							if ( editor != null ) {
+							root.add(node);
+							if (editor != null) {
 
 								editor.signals.sceneGraphChanged.dispatch();
 
@@ -125,9 +125,9 @@ class MetaFactory extends Factory {
 
 						}
 
-					} catch ( error ) {
+					} catch (error) {
 
-						console.error( error );
+						console.error(error);
 
 					}
 
@@ -140,96 +140,99 @@ class MetaFactory extends Factory {
 	}
 
 
-	async loadVoxel( url ) {
+	async loadVoxel(url) {
 
-		url = convertToHttps( url );
-		return new Promise( ( resolve, reject ) => {
+		url = convertToHttps(url);
+		return new Promise((resolve, reject) => {
 
 			const loader = new VOXLoader();
 			loader.load(
 				url,
-				function ( chunks ) {
+				function (chunks) {
 
-					const chunk = chunks[ 0 ];
-					const mesh = new VOXMesh( chunk );
+					const chunk = chunks[0];
+					const mesh = new VOXMesh(chunk);
 
 					//mesh.scale.set(0.005, 0.005, 0.005)
-					resolve( mesh );
+					resolve(mesh);
 
 				}
 			);
 
-		}, function ( xhr ) {
+		}, function (xhr) {
 
-			console.log( ( xhr.loaded / xhr.total ) * 100 + '% loaded!' );
+			console.log((xhr.loaded / xhr.total) * 100 + '% loaded!');
 
-		}, function ( error ) {
+		}, function (error) {
 
-			reject( error );
-			alert( error );
-			console.error( 'An error happened' );
+			reject(error);
+			alert(error);
+			console.error('An error happened');
 
 		}
 		);
 
 	}
 
-	async loadPolygen( url ) {
+	async loadPolygen(url) {
 
-		url = convertToHttps( url );
+		url = convertToHttps(url);
 		const self = this;
-		return new Promise( ( resolve, reject ) => {
+		return new Promise((resolve, reject) => {
 
-			const loader = new GLTFLoader( THREE.DefaultLoadingManager );
+			const loader = new GLTFLoader(THREE.DefaultLoadingManager);
 			const dracoLoader = new DRACOLoader();
 
-			dracoLoader.setDecoderPath( './draco/' );
-			loader.setDRACOLoader( dracoLoader );
+			dracoLoader.setDecoderPath('./draco/');
+			loader.setDRACOLoader(dracoLoader);
 
 			try {
 
 				loader.load(
 					url,
-					function ( gltf ) {
+					function (gltf) {
 
-						gltf.scene.children.forEach( item => {
+						gltf.scene.children.forEach(item => {
 
-							self.lockNode( item );
+							self.lockNode(item);
 
-						} );
-						resolve( gltf.scene );
+						});
+						resolve(gltf.scene);
 
 					},
-					function ( xhr ) {
+					function (xhr) {
 						//console.log((xhr.loaded / xhr.total) * 100 + '% loaded!')
 					},
-					function ( error ) {
+					function (error) {
 
-						resolve( null );
-						console.error( 'An error happened' );
+						resolve(null);
+						console.error('An error happened');
 
 					}
 				);
 
-			} catch ( error ) {
+			} catch (error) {
 
-				resolve( null );
-				console.error( error );
+				resolve(null);
+				console.error(error);
 
 			}
 
-		} );
+		});
 
 	}
-	async getPolygen( data, resources ) {
+	async getPhototype(data, resources) {
+
+		return null;
+	}
+	async getPolygen(data, resources) {
 
 
-		if ( resources.has( data.parameters.resource.toString() ) ) {
+		if (resources.has(data.parameters.resource.toString())) {
 
 
-			const resource = resources.get( data.parameters.resource.toString() );
-
-			const node = await this.loadPolygen( resource.file.url );
+			const resource = resources.get(data.parameters.resource.toString());
+			const node = await this.loadPolygen(resource.file.url);
 
 
 			return node;
@@ -240,89 +243,89 @@ class MetaFactory extends Factory {
 
 	}
 
-	async getPlane( url, width, height ) {
+	async getPlane(url, width, height) {
 
-		url = convertToHttps( url );
-		return new Promise( resolve => {
+		url = convertToHttps(url);
+		return new Promise(resolve => {
 
-			const geometry = new THREE.PlaneGeometry( width, height );
+			const geometry = new THREE.PlaneGeometry(width, height);
 			const loader = new THREE.TextureLoader();
-			loader.load( url, texture => {
+			loader.load(url, texture => {
 
-				const material = new THREE.MeshBasicMaterial( {
+				const material = new THREE.MeshBasicMaterial({
 					color: 0xffffff,
 					side: THREE.DoubleSide,
 					map: texture
-				} );
-				resolve( new THREE.Mesh( geometry, material ) );
+				});
+				resolve(new THREE.Mesh(geometry, material));
 
-			}, function ( xhr ) {
+			}, function (xhr) {
 				//console.log((xhr.loaded / xhr.total) * 100 + '% loaded!')
-			}, function ( error ) {
+			}, function (error) {
 
-				console.error( error );
-				resolve( new THREE.Mesh( geometry ) );
+				console.error(error);
+				resolve(new THREE.Mesh(geometry));
 
-			} );
+			});
 
-		} );
+		});
 
 	}
 
-	async getPicture( data, resources ) {
+	async getPicture(data, resources) {
 
-		const resource = resources.get( data.parameters.resource.toString() );
-		const info = JSON.parse( resource.info );
+		const resource = resources.get(data.parameters.resource.toString());
+		const info = JSON.parse(resource.info);
 		const size = info.size;
 		const width = data.parameters.width;
-		const height = width * ( size.y / size.x );
-		const node = await this.getPlane( resource.image.url, width, height );
+		const height = width * (size.y / size.x);
+		const node = await this.getPlane(resource.image.url, width, height);
 
 		return node;
 
 	}
-	async getEntity( data, resources ) {
+	async getEntity(data, resources) {
 
 		const entity = new THREE.Group();
 		entity.name = data.parameters.name;
 		return entity;
 
 	}
-	async getText( data, resources ) {
+	async getText(data, resources) {
 
 		const text = data.parameters.text;
-		console.error("text",data);
+		console.error("text", data);
 
 		const geometry = new THREE.PlaneGeometry(
-			(0.1 * text.length + 0.05)*0.21,
-			(0.1 + 0.05)*0.8
+			(0.1 * text.length + 0.05) * 0.21,
+			(0.1 + 0.05) * 0.8
 		);
-		const material = new THREE.MeshBasicMaterial( {
+		const material = new THREE.MeshBasicMaterial({
 			color: 0x8888ff,
 			side: THREE.DoubleSide,
-		} );
-		const plane = new THREE.Mesh( geometry, material );
+		});
+		const plane = new THREE.Mesh(geometry, material);
 
 		plane.name = data.parameters.name + '[text]';
 		return plane;
 
 	}
-	async getVoxel( data, resources ) {
+	async getVoxel(data, resources) {
 
 
-		if ( resources.has( data.parameters.resource.toString() ) ) {
+		if (resources.has(data.parameters.resource.toString())) {
 
-			const resource = resources.get( data.parameters.resource.toString() );
-			return await this.loadVoxel( resource.file.url );
+			const resource = resources.get(data.parameters.resource.toString());
+			return await this.loadVoxel(resource.file.url);
 
 		}
 
 		return null;
 
 	}
-	async getSound( data, resources ) {
+	async getSound(data, resources) {
 
-		if ( resources.has( data.parameters.resource.toString() ) ) {
+		if (resources.has(data.parameters.resource.toString())) {
 
 			const entity = new THREE.Group();
 			entity.name = data.parameters.name;
@@ -333,100 +336,101 @@ class MetaFactory extends Factory {
 		return null;
 
 	}
-	async getEmpty( data, resources ) {
+	async getEmpty(data, resources) {
 
 		const entity = new THREE.Group();
 		entity.name = data.parameters.name;
 		return entity;
 
 	}
-	async getVideo( data, resources ) {
+	async getVideo(data, resources) {
 
-		if ( data.parameters.resource == undefined ) {
+		if (data.parameters.resource == undefined) {
 
 			return null;
 
 		}
 
-		const resource = resources.get( data.parameters.resource.toString() );
-		const info = JSON.parse( resource.info );
+		const resource = resources.get(data.parameters.resource.toString());
+		const info = JSON.parse(resource.info);
 		const size = info.size;
 		const width = data.parameters.width;
-		const height = width * ( size.y / size.x );
-		return await this.getPlane( resource.image.url, width, height );
+		const height = width * (size.y / size.x);
+		return await this.getPlane(resource.image.url, width, height);
 
 	}
 
 
-	async building( data, resources ) {
+	async building(data, resources) {
 
-		console.log( 'building: ', data.parameters );
+		console.log('building: ', data.parameters);
 		let node = null;
-		switch ( data.type.toLowerCase() ) {
+		switch (data.type.toLowerCase()) {
 
 			case 'polygen':
-				node = await this.getPolygen( data, resources );//resource
+				node = await this.getPolygen(data, resources);//resource
 				break;
 			case 'picture':
-				node = await this.getPicture( data, resources );//resource
+				node = await this.getPicture(data, resources);//resource
 				break;
 			case 'video':
-				node = await this.getVideo( data, resources );//resource
+				node = await this.getVideo(data, resources);//resource
 				break;
 			case 'sound':
-				node = await this.getSound( data, resources );//resource
+				node = await this.getSound(data, resources);//resource
 				break;
 			case 'voxel':
-				node = await this.getVoxel( data, resources );//resource
+				node = await this.getVoxel(data, resources);//resource
 				break;
 			case 'text':
-				node = await this.getText( data, resources );
+				node = await this.getText(data, resources);
 				break;
 			case 'entity':
-				node = await this.getEntity( data, resources );
+				node = await this.getEntity(data, resources);
 				break;
 			case 'anchor':
-				node = await this.addAnchor( data );
+				node = await this.addAnchor(data);
 				break;
+
 
 		}
 
-		if ( node == null ) {
+		if (node == null) {
 
-			node = await this.getEmpty( data, resources );
+			node = await this.getEmpty(data, resources);
 
 		}
 
 		node.type = data.type;
 		node.name = data.parameters.name;
 		node.uuid = data.parameters.uuid;
-		this.setTransform( node, data.parameters.transform );
+		this.setTransform(node, data.parameters.transform);
 		node.visible = data.parameters.active;
 
 		const userData = { 'type': data.type };
-		const exclude = [ 'name', 'uuid', 'transform', 'active' ];
+		const exclude = ['name', 'uuid', 'transform', 'active'];
 
-		Object.keys( data.parameters ).forEach( key => {
+		Object.keys(data.parameters).forEach(key => {
 
-			if ( ! exclude.includes( key ) ) {
+			if (!exclude.includes(key)) {
 
-				userData[ key ] = data.parameters[ key ];
+				userData[key] = data.parameters[key];
 
 			}
 
-		} );
+		});
 
 		// 设置components和commands
 		node.components = data.children.components || [];
 		node.commands = data.children.commands || [];
 
 		node.userData = userData;
-		for ( let i = 0; i < data.children.entities.length; ++ i ) {
+		for (let i = 0; i < data.children.entities.length; ++i) {
 
-			const child = await this.building( data.children.entities[ i ], resources );
-			if ( child != null ) {
+			const child = await this.building(data.children.entities[i], resources);
+			if (child != null) {
 
-				node.add( child );
+				node.add(child);
 
 			}
 
